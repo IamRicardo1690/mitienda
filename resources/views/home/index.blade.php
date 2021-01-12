955b608959ffb623d0e99cc2a43d2b89758ab8eb
RicardoSanchezMarcos <rikirikon169@outlook.es>
2021-01-12 12:09:15 -0600

    subido_1

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Digital Service</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1" shrink-to-fit="no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- favicons -->
+<link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+<!-- favicons -->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/custom-responsive-style.css">
+<script type="text/javascript" src="scripts/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="scripts/plugin-active.js"></script>
+</head>
+<body>
+  <header>
+    <div class="mobile-logo">
+    <a href=""><img src="images/o.png"></a>
+  </div>
+  <a class="menu-toggle rounded" href="#">
+    <i class="fa fa-bars"></i>
+  </a>
+  <nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+      <li class="sidebar-brand">
+        <a class="smooth-scroll" href="#Header"></a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Services">Acerca de IamRicardo</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#OurWork">Nuestro equipo</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#OurBlog">Tienda</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Testimonials">Software</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Contact">Contacto</a>
+      </li>
+    </ul>
+  </nav>
+  </header>
+<section id="Banner">
+  <div class="logo">
+    <img src="images/logos/logo_tienda2.png">
+  </div>
+  <div class="blacksection">
+    <h1>Tecnologias<br>para la<br>Creatividad</h1>
+  </div>
+</section>
+<a href="#Services" class="mscroll"><img src="images/mouse-icon.png" alt="mouse icon"></a>
+<section id="Services">
+  <div class="container">
+    <h2>Nuestros servicios de software en tienda online </h2>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <div class="each-services">
+          <img src="images/logos/tienda.png" alt="Services">
+          <h3>Mercado Digital</h3>
+          <p>IamRicardo te ofrece varios productos en venta, puedes consultarlos en esta pagina en el apartado de productos. Electronicos, moda, accesorios, libros, videojugos, herramientas y mucho mas.</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <div class="each-services">
+          <img src="images/logos/software.png" alt="Services">
+          <h3>Desarrollo de paginas web</h3>
+          <p>Nos adaptamos a tus ideas para que lleves tu negocio al internet. Si necesitas una pagina web para tu empresa nosotros te brindamos desarrollo y mantenimiento para tu pagina web.</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <div class="each-services">
+          <img src="images/logos/reparacion.png" alt="Services">
+          <h3>Servicios de Instalacion de Software</h3>
+          <p> Si necesitas algun servicio para tu equipo de computo o celular, nosotros te brindamos ayuda para correguir cualquier problemas con tus equipos.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="OurWork">
+  <div class="container">
+    <h2>Nustro trabajo</h2>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+        <div class="grey-box">
+          <img src="images/logos/logo_tienda2.png" alt="Sponser">
+          <div class="sponser">
+            <h4>PATROCINADORES</h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
+        <div class="each-work space">
+          <img src="images/tienda.png" alt="Bearbrand">
+        </div>
+         <div class="each-work">
+          <img src="images/tienda.png" alt="Designer">
+        </div>
+        <div class="each-work space">
+          <img src="images/tienda.png" alt="Mountain">
+        </div>
+         <div class="each-work">
+          <img src="images/tienda.png" alt="Brandname">
+        </div>
+        <div class="each-work space">
+          <img src="images/tienda.png" alt="Stores">
+        </div>
+         <div class="each-work">
+          <img src="images/tienda.png" alt="Hipster">
+        </div>
+      </div>
+    </div>
+  </div>
+</section> 
+<section id="OurBlog"> 
+  <div class="container">
+    <h2>Tienda</h2>
+    <p>Productos en ofertas<br /> electronica, computo, moda y mas.</p>
+    <div class="blog-wrap">
+      <div class="each-block clearfix one"> 
+        <div class="icon">
+          <img src="images/tienda/samsung.jpg" width="300" height="200" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B08FBSZ2LX/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08FBSZ2LX&linkCode=as2&tag=iamricardo169-20&linkId=149003a876492cdf9915ca74bed1b3ef" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $8,998.00 Envío GRATIS</h3>
+          <p>Tv Samsung Crystal 4K UHD 43" Smart Tv UN43TU6900FXZX (2020)</p>
+        </div>
+      </div>
+      <div class="each-block clearfix two"> 
+        <div class="icon">
+          <img src="images/tienda/mesa.jpg" width="300" height="300" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B07Y5H3BFC/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07Y5H3BFC&linkCode=as2&tag=iamricardo169-20&linkId=531a0d1e38d079c64c8b7362990f5e92" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $769.00 Envío GRATIS. </h3>
+          <p>TECHVIDA Mesa Laptop, Bastidor de Enfriamiento para Computadora Portátil , Soporte de Aluminio Ajustable Portátil, Doble Ventilador y Tablero para Mouse, Portátil Universal, Adecuado para Sofás, Camas, etc, Diseño Ergonómico (Negro)</p>
+        </div>
+      </div>
+      <div class="each-block clearfix three"> 
+        <div class="icon">
+          <img src="images/tienda/control.jpg" width="300" height="200" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B08H7JXVHW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08H7JXVHW&linkCode=as2&tag=iamricardo169-20&linkId=815c56209b3bac31183b7cab213a9d6f" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $1,619.00 Envío GRATIS </h3>
+          <p>Sony Interactive Entertainment LLCPs5 Dualsense_LatControl Inalámbrico DualSense - Standard Edition</p>
+        </div>
+      </div>
+      <div class="each-block clearfix fourt"> 
+        <div class="icon">
+          <img src="images/tienda/silla.jpg" width="300" height="200" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B08NFJF4B1/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08NFJF4B1&linkCode=as2&tag=iamricardo169-20&linkId=a287a1069e151670c7a43cbee4d68459" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $1,199.00 Envío GRATIS </h3>
+          <p>TOPLIVING Silla para Oficina Operativa Silla Economica Ergonomica Silla Ejecutiva (Gris)</p>
+        </div>
+      </div>
+      <div class="each-block clearfix five"> 
+        <div class="icon">
+          <img src="images/tienda/juego.jpg" width="300" height="300" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B08HTF5KKB/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08HTF5KKB&linkCode=as2&tag=iamricardo169-20&linkId=041e1a34c4501dd5f3f450e096f83ce2" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $1,366.15 + $78.67 de envío </h3>
+          <p>Immortals Fenyx Rising PlayStation 5 Standard Edition</p>
+        </div>
+      </div>
+      <div class="each-block clearfix six"> 
+        <div class="icon">
+          <img src="images/tienda/juego.jpg" width="300" height="300" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B08HTF5KKB/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08HTF5KKB&linkCode=as2&tag=iamricardo169-20&linkId=041e1a34c4501dd5f3f450e096f83ce2" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $1,366.15 + $78.67 de envío </h3>
+          <p>Immortals Fenyx Rising PlayStation 5 Standard Edition</p>
+        </div>
+      </div>
+      <div class="each-block clearfix seven"> 
+        <div class="icon">
+          <img src="images/tienda/silla.jpg" width="300" height="200" border="0" alt="" style="border:none !important; margin:0px !important;">
+        </div>
+        <div class="blog-intro"> 
+          <br>
+          <a class="btn btn-warning" href="https://www.amazon.com.mx/gp/product/B08NFJF4B1/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08NFJF4B1&linkCode=as2&tag=iamricardo169-20&linkId=a287a1069e151670c7a43cbee4d68459" title="Comprar en Amazon" target="_blank" rel="nofollow">Comprar en Amazon</a>
+          <h3>Precio $1,199.00 Envío GRATIS </h3>
+          <p>TOPLIVING Silla para Oficina Operativa Silla Economica Ergonomica Silla Ejecutiva (Gris)</p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="learnmore">
+     
+    </div>
+    
+  </div>
+</section>
+<section id="Testimonials">
+   <div class="container">
+    <h2>Desarrollo de paginas web</h2>
+    <p>Si estas buscando llevar tu negocio al internet y obtener ganancias, nosotros te desarrollamos y administramos tu pagina web.</p>
+    <div class="testi-wrapper">
+        <div class="latest-testimonials each-testi">
+          <div class="division">
+            <span>03.</span> 
+            <div class="title">
+              <h4>Paginas web ejemplos</h4>
+              <h6>Tienda en linea..</h6>
+            </div>
+          </div>
+          <p>Vende tus productos sin necesidad de salir de casa!</p>
+        </div>
+        <div class="old-testimonials each-testi">
+          <p>Sistema para el registro y administracion de alumnos de la Escuela Preparatoria Oficial Núm 41.</p>
+          <div class="profile">
+            <img src="images/user-profile-one.png" alt="">
+            <div class="designation">
+              <h6>Preparatoria Oficial Núm 41.</h6>
+              <p>Amanalco Estado de Mexico</p>
+            </div>
+          </div>
+        </div>
+        <div class="old-testimonials each-testi no-margin">
+          <p>Sistema de inventario para la adminstracion de una biblioteca desarrollado con el lenguaje php en modelo visto controlador.</p>
+          <div class="profile">
+            <img src="images/user-profile-two.png" alt="">
+            <div class="designation">
+              <h6>Uriel Cipriano</h6>
+              <p>Sistema de inventariado</p>
+            </div>
+          </div>
+        </div>
+      </div>
+   </div>
+</section>
+<footer id="Contact">
+  <div class="container">
+    <p>Si tienes un proyecto apasionante, sientete libre de ponerse en contacto con nosotros</p>
+    <a href="mailto:iamricardo169@gmail.com" class="email-btn"><span><i class="fa fa-envelope-o" aria-hidden="true"></i> Email Me</span></a>
+    <ul class="social-icons">
+      <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+      <li><a href=""><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+      <li><a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+    </ul>
+  </div>
+</footer>
+ <div class="copyright"><p>iamricardo 2021</p>
+</div>
+</body>
+</html>
